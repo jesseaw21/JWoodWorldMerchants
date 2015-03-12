25e1618544fe482603799648a2f16900d23789b8
jesseaw21 <jessewood21@gmail.com>
2015-03-11 20:02:45 -0700

    Lab 8 Release Version

--- WorldOfMerchants/WorldMerchants.UnitTests/ItemsCrud_Tests.cs
+++ WorldOfMerchants/WorldMerchants.UnitTests/ItemsCrud_Tests.cs
@@ -13,6 +13,7 @@ namespace WorldMerchants.UnitTests
     public class ItemsCrud_Tests
     {
         const string ITEM1_NAME = "Goldenrod";
+        const string ITEM2_NAME = "Scythe";
         const string RARE = "Rare";
         const string MERCHANT = "Underhill";
         const string TYPE = "Metal";
@@ -23,7 +24,9 @@ namespace WorldMerchants.UnitTests
             // arrange
             List<Item> items = new List<Item>();
             Item item = new Item { Name = ITEM1_NAME };
+            Item item2 = new Item { Name = ITEM2_NAME };
             items.Add(item);
+            items.Add(item2);
 
             FakeUnitOfWork fakeUnit = new FakeUnitOfWork(null, items);
             ItemsCrudController target = new ItemsCrudController(fakeUnit);
@@ -34,6 +37,7 @@ namespace WorldMerchants.UnitTests
             // assert
             var model = (List<Item>)view.Model;
             Assert.AreEqual(ITEM1_NAME, model[0].Name);
+            Assert.AreEqual(ITEM2_NAME, model[1].Name);
         }
 
         [TestMethod]
@@ -42,17 +46,21 @@ namespace WorldMerchants.UnitTests
             // arrange
             List<Item> items = new List<Item>();
             ItemCreateViewModel item = new ItemCreateViewModel { Name = ITEM1_NAME, Value = 12, Points = 4 };
+            ItemCreateViewModel item2 = new ItemCreateViewModel { Name = ITEM2_NAME };
             List<Merchant> merchants = new List<Merchant>();
-            merchants.Add(new Merchant { Name = MERCHANT });
+            merchants.Add(new Merchant { ID = 1, Name = MERCHANT });
 
             FakeUnitOfWork fakeUnit = new FakeUnitOfWork(merchants, items);
             ItemsCrudController target = new ItemsCrudController(fakeUnit);
 
             // act
             target.Create(item, RARE, MERCHANT, TYPE);
+            target.Create(item2, RARE, MERCHANT, TYPE);
 
             // assert
             Assert.AreEqual(ITEM1_NAME, items[0].Name);
+            Assert.AreEqual(ITEM2_NAME, items[1].Name);
+            Assert.AreEqual(1, items[0].MerchantID);
         }
 
     }
